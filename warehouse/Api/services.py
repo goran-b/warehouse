2625ae43ed1891eaf2583390c23fdfb82754a518
goran-b <goran.b@mail.com>
2020-08-14 17:00:28 +0200

    fixed spaghetti code

--- warehouse/Api/services.py
+++ warehouse/Api/services.py
@@ -1,68 +1,91 @@
 import requests
 import json
 from .models import RecentPurchase
+from rest_framework.exceptions import NotFound
+
+recent_purchases = []
+username_of_user = str
+
+def throw_error_no_user(username,user_response):
+    if len(user_response)==0:
+        raise NotFound('User with username of ' + username + ' was not found!')
+
+def get_all_purchases_by_user(username):
+    list_recent_purchases = []
+    list_recent_purchases = SendRequest.send_request_user_purcheses(username)
+
+    for x in list_recent_purchases:
+        productId = x['productId']
+        add_RecentPurchase_to_list(get_RecentPurchase(productId))
+    sort_purchases_by_most_buyers()
+    return recent_purchases
 
 
-class RecentPurchases:
-    def __init__(self,username,recent_purchases = []):
-        self.username= username
-        self.recent_purchases = recent_purchases
-        
-        
-        def get_recent_purchases_user(username):
-            response_recent_purchases_user=requests.get('http://127.0.0.1:4000/api/purchases/by_user/'+ username+'?limit=5')
-            list_recent_purchases= response_recent_purchases_user.json()['purchases']
+def sort_purchases_by_most_buyers():
+    recent_purchases.sort(key=lambda x: len(x.recent), reverse=True)
 
-            for x in list_recent_purchases:
-                productId=x['productId']
-                get_data_purchases(productId) 
+class SendRequest():
 
-            recent_purchases.sort(key=lambda x: len(x.recent), reverse=True)
-            newlist = sorted(recent_purchases,key=lambda x: len(x.recent), reverse=False)
-        
-                
-        def get_data_purchases(id):
-            product_details=get_product_details(id)
-            other_buyers_of_product=get_other_buyers_of_product(id)
-                     
-            recent_purchase = RecentPurchase(
-                id=product_details['id'],
-                face=product_details['face'], 
-                price=product_details['price'], 
-                size=product_details['size'],
-                recent=other_buyers_of_product)
+    API_URL = 'http://127.0.0.1:4000/api/'
 
-            recent_purchases.append(recent_purchase)
-            
+    @classmethod
+    def send_request_user_purcheses(cls, username):
+        response = requests.get(
+            f"{cls.API_URL}purchases/by_user/{username}?limit=5")
+        data = response.json()['purchases']
+        throw_error_no_user(username,data)
+        return data
 
-        def get_product_details(id):
-            response_product_details=requests.get('http://127.0.0.1:4000/api/products/'+ str(id))         
-            product_details = response_product_details.json()['product']
-            return product_details
+    @classmethod
+    def send_request_product_details(cls, productId):
+        response = requests.get(
+            f"{cls.API_URL}products/{str(productId)}")
+        data = response.json()['product']
+        return data
+
+    @classmethod
+    def send_request_buyer_of_product(cls, productId):
+        response = requests.get(
+            f"{cls.API_URL}purchases/by_product/{str(productId)}")
+        data = response.json()['purchases']
+        return data
 
 
-        def get_other_buyers_of_product(id):
-            response_other_buyers_of_product=requests.get('http://127.0.0.1:4000/api/purchases/by_product/'+ str(id))         
-            other_buyers_of_product = response_other_buyers_of_product.json()['purchases']
-            list_of_other_buyers=[]
-            for e in other_buyers_of_product:
-                if e['username'] != self.username:
-                    list_of_other_buyers.append(e['username'])   
-            return list_of_other_buyers
+def remove_username_from_other_buyers(username, list_of_other_buyers):
+    data = []
+    for buyer in list_of_other_buyers:
+        if buyer['username'] != username_of_user:
+            data.append(buyer['username'])
+    return data
 
 
-        def main(self,username):
-            get_recent_purchases_user(username) 
-       
-
-        main(self,username)
-                
-
-     
-
-
-   
+def get_RecentPurchase(productId):
+    product_details = SendRequest.send_request_product_details(productId)
+    buyers_of_product = SendRequest.send_request_buyer_of_product(
+        productId)
+    list_of_other_buyers = remove_username_from_other_buyers(
+        username_of_user, buyers_of_product)
+    recent_purchase = create_RecentPurchase(
+        product_details, list_of_other_buyers)
+    return recent_purchase
 
 
+def create_RecentPurchase(product_details, list_of_other_buyers):
+    recent_purchase = RecentPurchase(
+        id=product_details['id'],
+        face=product_details['face'],
+        price=product_details['price'],
+        size=product_details['size'],
+        recent=list_of_other_buyers)
+    return recent_purchase
 
-    
+
+def add_RecentPurchase_to_list(recent_purchase: RecentPurchase):
+    recent_purchases.append(recent_purchase)
+
+
+def get_recent_purchases(username):
+    recent_purchases.clear()
+    username_of_user = username
+    data = get_all_purchases_by_user(username)
+    return data
--- warehouse/Api/views.py
+++ warehouse/Api/views.py
@@ -1,21 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import  RecentPurchaseSerializer
-from .models import RecentPurchase
-from .services import RecentPurchases
-from rest_framework.exceptions import NotFound
-
+from .serializers import RecentPurchaseSerializer
+from .services import get_recent_purchases, throw_error_no_user
 
 
 @api_view()
-def recent_purchases_view(request,pk):
-    recent_purchases = []
-    username=str(pk)
-    recent_purchases=RecentPurchases(username).__dict__['recent_purchases']
-    recent_purchases = list(dict.fromkeys(recent_purchases))
-    if len(recent_purchases)==0:
-        raise NotFound('User with username of ' + username + ' was not found!')
-    serializer = RecentPurchaseSerializer(recent_purchases,many=True)
+def recent_purchases_view(request, pk):
+    username = str(pk)
+    recent_purchases = get_recent_purchases(username)
+    serializer = RecentPurchaseSerializer(recent_purchases, many=True)
     return Response(serializer.data)
-   
-        
